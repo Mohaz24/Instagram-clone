a03ed41f0c44293819bece9fa3658b6d3c6a182f
Mohammed <Mohazxsk@gmail.com>
2023-03-06 22:16:37 +1100

    new updates

--- index.js
+++ index.js
@@ -1,36 +1,74 @@
 const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+  {
+    name: "Vincent van Gogh",
+    username: "vincey1853",
+    location: "Zundert, Netherlands",
+    avatar: "images/avatar-vangogh.jpg",
+    post: "images/post-vangogh.jpg",
+    comment: "just took a few mushrooms lol",
+    likes: 100
+  },
+  {
+    name: "Gustave Courbet",
+    username: "gus1819",
+    location: "Ornans, France",
+    avatar: "images/avatar-courbet.jpg",
+    post: "images/post-courbet.jpg",
+    comment: "i'm feelin a bit stressed tbh",
+    likes: 500,
+  },
+  {
+    name: "Joseph Ducreux",
+    username: "jd1735",
+    location: "Paris, France",
+    avatar: "images/avatar-ducreux.jpg",
+    post: "images/post-ducreux.jpg",
+    comment:
+      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+    likes: 50,
+  },
+];
 
-// const likes = document.querySelector(".heart")
-// let count = 0;
-// likes.addEventListener("click", () => {
-//     count += 1
-// })
+const container = document.querySelector(".posts");
+console.log(posts);
 
+let elements = ``;
+posts.forEach((posters) => {
+  //console.log(posters)
+  elements += `
+  
+  <section>
+      <div class="container">
+        <div class="flex-box">
+          <div>
+            <img src="${posters.avatar}" class="avatar" />
+          </div>
+          <div>
+            <span class="user title">${posters.name}</span>
+            <span class="user  sub-title">${posters.location}</span>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <img src="${posters.post}" class="section-post" />
+      </div>
+    </section>
+    <footer>
+      <div class="container">
+        <div class="flex">
+          <img src="images/icon-heart.png" class="icon heart" />
+          <img src="images/icon-comment.png" class="icon" />
+          <img src="images/icon-dm.png" class="icon" />
+        </div>
+        <p class="likes">${posters.likes} likes</p>
+        <p class="comment">
+          <span class="bold">${posters.username}</span>  ${posters.comment}
+        </p>
+      </div>
+    </footer>
+  `;
+});
+
+container.innerHTML = elements;
