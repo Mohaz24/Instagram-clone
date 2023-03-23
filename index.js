c4a4f02d7d60dec47a67233200b8258c556c0d38
Mohammed <Mohazxsk@gmail.com>
2023-03-23 16:05:30 +1100

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const posts = [
     avatar: "images/avatar-vangogh.jpg",
     post: "images/post-vangogh.jpg",
     comment: "just took a few mushrooms lol",
-    likes: 100
+    likes: 710,
   },
   {
     name: "Gustave Courbet",
@@ -15,7 +15,7 @@ const posts = [
     avatar: "images/avatar-courbet.jpg",
     post: "images/post-courbet.jpg",
     comment: "i'm feelin a bit stressed tbh",
-    likes: 500,
+    likes: 980,
   },
   {
     name: "Joseph Ducreux",
@@ -25,50 +25,58 @@ const posts = [
     post: "images/post-ducreux.jpg",
     comment:
       "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-    likes: 50,
+    likes: 450,
   },
 ];
 
 const container = document.querySelector(".posts");
 console.log(posts);
 
-let elements = ``;
-posts.forEach((posters) => {
-  //console.log(posters)
-  elements += `
-  
-  <section>
-      <div class="container">
-        <div class="flex-box">
-          <div>
-            <img src="${posters.avatar}" class="avatar" />
-          </div>
-          <div>
-            <span class="user title">${posters.name}</span>
-            <span class="user  sub-title">${posters.location}</span>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section>
-      <div class="container">
-        <img src="${posters.post}" class="section-post" />
-      </div>
-    </section>
-    <footer>
-      <div class="container">
-        <div class="flex">
-          <img src="images/icon-heart.png" class="icon heart" />
-          <img src="images/icon-comment.png" class="icon" />
-          <img src="images/icon-dm.png" class="icon" />
-        </div>
-        <p class="likes">${posters.likes} likes</p>
-        <p class="comment">
-          <span class="bold">${posters.username}</span>  ${posters.comment}
-        </p>
-      </div>
-    </footer>
-  `;
-});
 
-container.innerHTML = elements;
+
+function render(){
+
+    let elements = ``;
+    posts.forEach((posters) => {
+      //console.log(posters)
+      elements += `
+      
+      <section>
+          <div class="container">
+            <div class="flex-box">
+              <div>
+                <img src="${posters.avatar}" class="avatar" />
+              </div>
+              <div>
+                <span class="user title">${posters.name}</span>
+                <span class="user  sub-title">${posters.location}</span>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div class="container">
+            <img src="${posters.post}" class="section-post" />
+          </div>
+        </section>
+        <footer>
+          <div class="container">
+            <div class="flex">
+            <img src="images/heart.png"  class="icon"  /> 
+              <img src="images/icon-comment.png" class="icon" />
+              <img src="images/icon-dm.png" class="icon" />
+            </div>
+            <p class="likes">${posters.likes} likes</p>
+            <p class="comment">
+              <span class="bold">${posters.username}</span>  ${posters.comment}
+            </p>
+          </div>
+        </footer>
+      `;
+    });
+    
+    return container.innerHTML = elements;
+}
+
+render()
+
